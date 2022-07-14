22d519fe2de42140b949cc1780b1ca24b30ded32
sacru2red <sacru2red@gmail.com>
2022-07-14 14:51:03 +0900

    add default export, index.d.ts, remove index.web.js

--- index.js
+++ index.js
@@ -1,45 +1,17 @@
-import { QuotaExceededError, TypeMismatchError } from './src/errors'
-
-const base64Decode = require('fast-base64-decode')
-const { NativeModules } = require('react-native')
-
-let warned = false
-function insecureRandomValues (array) {
-  if (!warned) {
-    console.warn('Using an insecure random number generator, this should only happen when running in a debugger without support for crypto.getRandomValues')
-    warned = true
-  }
-
-  for (let i = 0, r; i < array.length; i++) {
-    if ((i & 0x03) === 0) r = Math.random() * 0x100000000
-    array[i] = (r >>> ((i & 0x03) << 3)) & 0xff
-  }
-
-  return array
-}
-
-/**
- * @param {number} byteLength
- * @returns {string}
- */
-function getRandomBase64 (byteLength) {
-  if (NativeModules.RNGetRandomValues) {
-    return NativeModules.RNGetRandomValues.getRandomBase64(byteLength)
-  } else if (NativeModules.ExpoRandom) {
-    // Expo SDK 41-44
-    return NativeModules.ExpoRandom.getRandomBase64String(byteLength)
-  } else if (global.ExpoModules) {
-    // Expo SDK 45+
-    return global.ExpoModules.ExpoRandom.getRandomBase64String(byteLength)
-  } else {
-    throw new Error('Native module not found')
-  }
-}
-
-/**
- * @param {Int8Array|Uint8Array|Int16Array|Uint16Array|Int32Array|Uint32Array|Uint8ClampedArray} array
- */
 export function getRandomValues (array) {
+  // If you're running react-native debug mode (Chrome debug)
+  // calling request is replaced globalThis.crypto.getRandomValues
+  // If you ignore this, you may get error on getRandomBase64
+  if (globalThis.crypto) {
+    if (globalThis.crypto.getRandomValues) {
+      return globalThis.crypto.getRandomValues(array)
+    }
+  }
+
+  const { QuotaExceededError, TypeMismatchError } = require('./src/errors')
+  const { getRandomBase64, insecureRandomValues } = require('./src/lib')
+  const base64Decode = require('fast-base64-decode')
+
   if (!(array instanceof Int8Array || array instanceof Uint8Array || array instanceof Int16Array || array instanceof Uint16Array || array instanceof Int32Array || array instanceof Uint32Array || array instanceof Uint8ClampedArray)) {
     throw new TypeMismatchError('Expected an integer array')
   }
@@ -48,16 +20,9 @@ export function getRandomValues (array) {
     throw new QuotaExceededError('Can only request a maximum of 65536 bytes')
   }
 
-  // Calling getRandomBase64 in debug mode leads to the error
-  // "Calling synchronous methods on native modules is not supported in Chrome".
-  // So in that specific case we fall back to just using Math.random.
-  if (__DEV__) {
-    if (typeof global.nativeCallSyncHook === 'undefined') {
-      return insecureRandomValues(array)
-    }
-  }
-
   base64Decode(getRandomBase64(array.byteLength), new Uint8Array(array.buffer, array.byteOffset, array.byteLength))
 
   return array
 }
+
+export default getRandomValues
