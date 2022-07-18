df037c60e9ab65b6bda8556fbd9c864aa5e09899
sacru2red <sacru2red@gmail.com>
2022-07-18 19:30:06 +0900

    fix error on nodeJs

--- src/lib.ts
+++ src/lib.ts
@@ -1,12 +1,17 @@
+import { Environment } from '@react-native-module/utility'
+
 export function getRandomBase64 (byteLength: number): string {
   // For running on Nodejs - testing, console etc.
-  const isRunningOnReactNative = globalThis.navigator && globalThis.navigator.product && globalThis.navigator.product === 'ReactNative'
-  if (!isRunningOnReactNative) {
+  if (Environment !== 'NativeMobile') {
     try {
-      const crypto = globalThis.crypto
-      if (Buffer && crypto.webcrypto && crypto.webcrypto.getRandomValues) {
-        return Buffer.from(crypto.webcrypto.getRandomValues(new Uint8Array(byteLength))).toString('base64')
-      } else if (crypto.randomBytes) {
+      if (Environment === 'Browser') {
+        const crypto = globalThis.crypto
+        if (Buffer && crypto.webcrypto && crypto.webcrypto.getRandomValues) {
+          return Buffer.from(crypto.webcrypto.getRandomValues(new Uint8Array(byteLength))).toString('base64')
+        }
+      } else if (Environment === 'NodeJs') {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const crypto = require('node:crypto')
         return crypto.randomBytes(byteLength).toString('base64')
       }
     } catch (error) {
